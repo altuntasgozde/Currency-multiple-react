ea469f0c015459e84096c82ef4234002b506220a
Gözde Altuntaş <altuntasgozde@gmail.com>
2021-04-16 21:02:40 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [option, setOption] = useState();
+
+  const [data, setData] = useState();
+
+  const [newData, setNewData] = useState();
+
+  const ChangeValue = (event) => {
+    setData(event.target.value);
+  };
+
+  const ChangeButton = () => {
+    if (option === "USD") {
+      axios
+        .get("https://api.exchangerate.host/latest?base=TRY")
+        .then((response) => {
+          const rates = response.data.rates.USD;
+          setNewData(rates * data);
+        });
+    } else if (option === "EUR") {
+      axios
+        .get("https://api.exchangerate.host/latest?base=TRY")
+        .then((response) => {
+          const rates = response.data.rates.EUR;
+          setNewData(rates * data);
+        });
+    } else if (option === "GBP") {
+      axios
+        .get("https://api.exchangerate.host/latest?base=TRY")
+        .then((response) => {
+          const rates = response.data.rates.GBP;
+          setNewData(rates * data);
+        });
+    }
+  };
+
+  const ChangeOption = (event) => {
+    setOption(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <input type="text" value={data} onChange={ChangeValue}></input>
+      <label>TL</label>
+      <div className="radiobuttons" onChange={ChangeOption} value={option}>
+        <input type="radio" value="USD" name="currency" /> USD
+        <input type="radio" value="EUR" name="currency"/> EUR
+        <input type="radio" value="GBP" name="currency"/> GBP
+      </div>
+      <button onClick={ChangeButton}>Change</button>
+      <input type="text" value={newData}></input>
     </div>
   );
 }
